c0dc41ac58cb88f59258fc2a3b6ea17660777f1b
Kerby Keith Aquino <skepfoosky15@gmail.com>
2024-03-01 03:00:53 +0800

    minor: fix whitespace

--- src/routes/v1/Profile/controllers.ts
+++ src/routes/v1/Profile/controllers.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { FastifyReply, FastifyRequest } from "fastify"
 import { User } from "../../../models"
 
@@ -6,11 +5,12 @@ export const me = async (request: FastifyRequest, reply: FastifyReply) => {
   const user = request.user as { id: number }
 
   const userData = await request.server.db
-
     .getRepository(User)
     .findOne({ where: { id: user.id } })
+
   if (!userData) {
     return reply.code(404).send({ error: "User not found" })
   }
+
   return reply.code(200).send({ ...userData })
 }
