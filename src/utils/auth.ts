6d5c934ebb4e3616287e2a52ff4530adda439883
Jason Diaz Jimenez <developervulpo@outlook.com>
2024-03-02 20:04:23 -0600

    Add refresh token functionality to verifyToken function

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,39 @@
 import { FastifyReply, FastifyRequest } from "fastify"
 
 const verifyToken = async (request: FastifyRequest, reply: FastifyReply) => {
-  try {
-    const payload = await request.jwtVerify<{ id: string }>()
-    request.user = payload
-  } catch (error) {
+  const accessToken = request.cookies.accessToken
+  if (!accessToken) {
     return reply.code(401).send({ error: "Unauthorized" })
+  }
+
+  let payload
+
+  try {
+    payload = await request.server.jwt.verify(accessToken)
+  } catch (error) {
+    const refreshToken = request.cookies.refreshToken
+    if (!refreshToken) {
+      return reply.code(401).send({ error: "Unauthorized" })
+    }
+
+    try {
+      payload = await request.server.jwt.verify<{ id: string }>(refreshToken)
+      request.user = payload
+      // Send new access token
+      const newAccessToken = request.server.jwt.sign({ id: payload.id })
+      reply.setCookie("accessToken", newAccessToken, {
+        httpOnly: true,
+        path: "/",
+        sameSite: "strict"
+      })
+    } catch (error) {
+      reply.clearCookie("refreshToken").clearCookie("accessToken")
+      return reply.code(401).send({ error: "Unauthorized" })
+    }
+  } finally {
+    if (payload) {
+      request.user = payload
+    }
   }
 }
 
