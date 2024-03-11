e86cb6085a7fe1d4ef5c3d9b1c7013d92117584d
Jason Diaz Jimenez <developervulpo@outlook.com>
2024-03-10 22:16:49 -0500

    Added comment entity

---
+++ src/models/Comments.ts
@@ -0,0 +1,34 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { Character, User } from '.'
+import { Artwork } from './Artwork';
+
+@Entity()
+export class Comment {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    content: string;
+
+    @ManyToOne(() => User, { nullable: true })
+    user: User;
+
+    @ManyToOne(() => Artwork, { nullable: true })
+    artwork?: Artwork;
+
+    @ManyToOne(() => Character, { nullable: true })
+    character?: Character;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
