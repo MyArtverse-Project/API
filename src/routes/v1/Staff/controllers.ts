6f1b1ca6b6466ce1bf9d6e0bc3555d5763509857
Jason Diaz Jimenez <developervulpo@outlook.com>
2024-04-29 01:19:26 -0500

    Promote/Demote User Artist Status

--- src/routes/v1/Staff/controllers.ts
+++ src/routes/v1/Staff/controllers.ts
@@ -1,14 +1,40 @@
 import type { FastifyReply, FastifyRequest } from "fastify"
 import { User } from "../../../models"
 import { IsNull, Not } from "typeorm"
+import { sendNotification } from "../../../utils/notification"
 
-export const promoteUserToArtist = (_request: FastifyRequest, reply: FastifyReply) => {
-  return reply.code(200).send({ message: "Promoted User to artist" })
+export const promoteUserToArtist = async (request: FastifyRequest, reply: FastifyReply) => {
+  const { userId } = request.params as { userId: string }
+  const user = await request.server.db.getRepository(User).findOne({
+    where: {
+      id: userId
+    }
+  })
+
+  if (!user) return reply.code(404).send({ error: "User not found" })
+  user.hasArtistAccess = true
+  sendNotification(request.server.db, user, "Your artist application has been approved! You now have access to the artist features.")
+  await request.server.db.getRepository(User).save(user)
+  return reply.code(200).send({ message: "User promoted to artist" })
+}
+
+export const demoteUserToArtist = async (request: FastifyRequest, reply: FastifyReply) => {
+  const { userId } = request.params as { userId: string }
+  const user = await request.server.db.getRepository(User).findOne({
+    where: {
+      id: userId
+    }
+  })
+
+  if (!user) return reply.code(404).send({ error: "User not found" })
+  user.hasArtistAccess = false
+  await request.server.db.getRepository(User).save(user)
+  return reply.code(200).send({ message: "User demoted to artist" })
 }
 
 export const getArtistRequests = async (request: FastifyRequest, reply: FastifyReply) => {
   const requests = await request.server.db.getRepository(User).find({
-    where: { 
+    where: {
       hasArtistAccess: false,
       artistApplication: Not(IsNull())
     }
@@ -16,4 +42,16 @@ export const getArtistRequests = async (request: FastifyRequest, reply: FastifyR
 
   if (!requests) return reply.code(404).send({ error: "No artist requests found" })
   return reply.code(200).send(requests)
+}
+
+export const getRecentlyApprovedArtists = async (request: FastifyRequest, reply: FastifyReply) => {
+  const artists = await request.server.db.getRepository(User).find({
+    where: {
+      hasArtistAccess: true,
+      artistApplication: Not(IsNull())
+    }
+  })
+
+  if (!artists) return reply.code(404).send({ error: "No artists found" })
+  return reply.code(200).send(artists)
 }
--- src/routes/v1/Staff/routes.ts
+++ src/routes/v1/Staff/routes.ts
@@ -1,12 +1,10 @@
 import { type FastifyInstance } from "fastify"
-import { getArtistRequests, promoteUserToArtist } from "./controllers"
+import { demoteUserToArtist, getArtistRequests, getRecentlyApprovedArtists, promoteUserToArtist } from "./controllers"
 
 async function StaffRoutes(server: FastifyInstance) {
-  server.put(
-    "/update-artist",
-    { onRequest: [server.auth, server.permissionAboveMod] },
-    () => promoteUserToArtist
-  )
+  server.put("/promote/:userId/artist", { onRequest: [server.auth, server.permissionAboveMod] }, promoteUserToArtist)
+  server.put("/demote/:userId/artist", { onRequest: [server.auth, server.permissionAboveMod] }, demoteUserToArtist)
+  server.get('/recently-approved-artists', { onRequest: [server.auth, server.permissionAboveMod] }, getRecentlyApprovedArtists)
   server.get('/artist-requests', { onRequest: [server.auth, server.permissionAboveMod] }, getArtistRequests)
 }
 
